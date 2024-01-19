d493c9dd9748971449a1dae2d69f44168425e675
annasrees <asrees@uvm.edu>
2024-01-19 13:59:33 -0500

    defined Create_world() in generate.py

--- generate.py
+++ generate.py
@@ -1,24 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
-
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = .5
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-# pyrosim.Send_Cube(name="Box2", pos=[x + 1, y, z + 1] , size=[length, width, height])
-# for i in range(10):
-#     length = length * 0.9
-#     width = width * 0.9
-#     height = height * 0.9
-#     for j in range(5):
-#         for k in range(5):
-#             pyrosim.Send_Cube(name="Box" + str(i), pos=[x + j, y + k, z + i] , size=[length, width, height])
-            
 
-pyrosim.End()
+def Create_world():
+    pyrosim.Start_SDF("world.sdf") 
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])      
+    pyrosim.End()
 
+Create_world()
 
