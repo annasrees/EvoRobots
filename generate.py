03fa2fa8b3cefe81f27418c46b0abb2bb4937b81
annasrees <asrees@uvm.edu>
2024-01-22 08:36:01 -0500

    Added joints for link0_link1 and link1_link2

--- generate.py
+++ generate.py
@@ -15,9 +15,12 @@ def Create_world():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     '''Storing a description of the robot's body here'''
-    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,0.75])
-    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[length, width, height])
+    #come back to Module 3, instruction 31
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, z] , size=[length, width, height]) #torso L0
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0, 0, 1.0]) #torso_leg joint L0_L1
+    pyrosim.Send_Cube(name="Link1", pos=[x, y, z] , size=[length, width, height]) #leg L1
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1.0])
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z] , size=[length, width, height]) #L2
     pyrosim.End()
 
 Create_world()
