0023adbeff6e12f8d85cf003103e4bfa3f9c8ccc
annasrees <asrees@uvm.edu>
2024-02-01 09:47:59 -0500

    got the robot to show up and then disappear

--- simulation.py
+++ simulation.py
@@ -21,8 +21,8 @@ class SIMULATION:
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
         self.world = WORLD()
-        self.robot = ROBOT()
-        pyrosim.Prepare_To_Simulate(robot.robotId)
+        self.robot = ROBOT(2, 2)
+        pyrosim.Prepare_To_Simulate(self.robot.robotId) #this is supposed to be robotId
 
         '''
         29. cut the statements from simulate.py that load
