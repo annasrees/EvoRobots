7acaa713c24232ceb716e5975f4869388b1a6ad4
annasrees <asrees@uvm.edu>
2024-02-02 13:32:21 -0500

    completed functionality of refactored code

--- motor.py
+++ motor.py
@@ -10,21 +10,15 @@ import random
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
-        self.motorValues = []
+        # self.Prepare_To_Act()
+        # self.motorValues = []
 
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.offset = c.phaseOffset
-
-        for i in range(1000):
-          self.motorValues[i] = self.amplitude * np.sin(self.frequency * c.sinVals[i] + self.offset)
 
           
     def Set_Value(self, robot, t):
+        # self.motorValues[i] = robot.motors[i]
         pyrosim.Set_Motor_For_Joint(bodyIndex = robot.robotId,
                                     jointName = self.jointName,
                                     controlMode = p.POSITION_CONTROL,
-                                    targetPosition = self.motorValues[t],
+                                    targetPosition = robot.motorValues[t],
                                     maxForce = 500)
