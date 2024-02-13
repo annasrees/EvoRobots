25a0a0e1378fdd299265b9b068de3a594863449e
unknown <asrees@uvm.edu>
2024-02-13 16:51:48 -0500

    added solution.py

---
+++ search.py
@@ -0,0 +1,8 @@
+import os
+from hillclimber import HILL_CLIMBER
+# NOTE TO SELF: original instrictions were to add "python3 generate.py". Changed to py instead
+# for i in range(5):
+#     os.system("py generate.py")
+#     os.system("py simulate.py")
+
+hc = HILL_CLIMBER()
